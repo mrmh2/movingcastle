573ffc935ddf8123a272aef68939812feab31fda
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-03-30 13:11:22 +0100

    Updated paths in analyse.sh

--- movingcastle/analyse.sh
+++ movingcastle/analyse.sh
@@ -4,6 +4,6 @@ ARGS=$@
 PWD=`pwd`
 
 docker run --rm \
--v `pwd`/../movingcastle:/movingcastle \
+-v `pwd`/../movingcastle/movingcastle:/movingcastle \
 -v /var/run/docker.sock:/var/run/docker.sock \
 movingcastle /movingcastle/calcifer.py analyse $PWD $@
